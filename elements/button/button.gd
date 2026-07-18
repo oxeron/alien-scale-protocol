227ecd8932ba489900d8934792d99f25e44321bd
Olivier PIERRE <olivier.pierre@gmail.com>
2026-07-18 13:21:17 +0200

    Button can be always pressed Extensible platform

--- elements/button/button.gd
+++ elements/button/button.gd
@@ -5,19 +5,31 @@ extends Area2D
 @export var on_state_texture : Texture2D
 @export var target : Node2D
 
+@export var keep_pressed : bool = false
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
+
+var _is_pressed : bool = false
 
 func _ready() -> void:
 	sprite_2d.texture = off_state_texture
 
 func _on_body_entered(_body: Node2D) -> void:
+	if not keep_pressed and _is_pressed:
+		return
+		
 	sprite_2d.texture = on_state_texture
 	if target:
 		if target.has_method("activate"):
 			target.activate()
-
+			_is_pressed = true
+			
 func _on_body_exited(_body: Node2D) -> void:
+	if not keep_pressed and _is_pressed:
+		return
+	
 	sprite_2d.texture = off_state_texture
+	
 	if target:
 		if target.has_method("deactivate"):
 			target.deactivate()
--- elements/platforms/extensible_platform/extensible_platform.gd
+++ elements/platforms/extensible_platform/extensible_platform.gd
@@ -6,9 +6,16 @@ extends AnimatableBody2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
+func _ready() -> void:
+	collision_shape_2d.shape = collision_shape_2d.shape.duplicate()
+	
 # Called when platform should extend
 func activate() -> void:
 	sprite_2d.scale.x = 2
+	collision_shape_2d.shape.size.x *= 2
+	collision_shape_2d.position.x = collision_shape_2d.shape.size.x / 2
 
 func deactivate() -> void:
 	sprite_2d.scale.x = 1
+	collision_shape_2d.shape.size.x /= 2
+	collision_shape_2d.position.x = collision_shape_2d.shape.size.x * 2
