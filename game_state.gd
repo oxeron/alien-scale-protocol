3ed940da6cc530368ff7f33ace5e47900887b75b
Olivier PIERRE <olivier.pierre@gmail.com>
2026-07-18 16:20:38 +0200

    Game state player scale

--- game_state.gd
+++ game_state.gd
@@ -8,7 +8,11 @@ var energy : int = 0:
 	set(value):
 		energy = clamp(value, 0, 100)
 		energy_changed.emit(energy)
-	
+var player_scale: float = 1.0:
+	set(value):
+		player_scale = clamp(value, 0.1, 2.0)
+
+
 func register_retry() -> void:
 	energy = 0
 	retry_count += 1
