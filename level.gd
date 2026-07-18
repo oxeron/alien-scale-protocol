f5acd071652517c19546fd060721f6fa43f41afd
Olivier PIERRE <olivier.pierre@gmail.com>
2026-07-18 10:16:06 +0200

    Level Timer and Retry counter

---
+++ level.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var chrono: Label = $HUD/Chrono
+@onready var retries: Label = $HUD/Retries
+@onready var retry_button: TextureButton = $HUD/RetryButton
+
+var elapsed_time: float = 0.0
+var is_chrono_running: bool = true
+var retry_count: int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if is_chrono_running:
+		elapsed_time += delta
+		update_time_label()
+
+func update_time_label() -> void:
+	@warning_ignore("integer_division")
+	var minutes := int(elapsed_time) / 60
+	var seconds := int(elapsed_time) % 60
+	var milliseconds := int((elapsed_time - int(elapsed_time)) * 100)
+	chrono.text = "%02d:%02d.%02d" % [minutes, seconds, milliseconds]
+
+
+func _on_retry_button_pressed() -> void:
+	get_tree().reload_current_scene()
+	elapsed_time = 0.0
+	retry_count += 1
+	retries.text = "%02d Retry" % [retry_count]
