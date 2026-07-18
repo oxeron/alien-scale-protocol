24eca123bde5ada250be959e1a081186883c7de3
Olivier PIERRE <olivier.pierre@gmail.com>
2026-07-18 11:54:14 +0200

    Button target

--- elements/button/button.gd
+++ elements/button/button.gd
@@ -3,17 +3,21 @@ extends Area2D
 
 @export var off_state_texture : Texture2D
 @export var on_state_texture : Texture2D
+@export var target : Node2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	sprite_2d.texture = off_state_texture
 
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		sprite_2d.texture = on_state_texture
+func _on_body_entered(_body: Node2D) -> void:
+	sprite_2d.texture = on_state_texture
+	if target:
+		if target.has_method("activate"):
+			target.activate()
 
-
-func _on_body_exited(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		sprite_2d.texture = off_state_texture
+func _on_body_exited(_body: Node2D) -> void:
+	sprite_2d.texture = off_state_texture
+	if target:
+		if target.has_method("deactivate"):
+			target.deactivate()
