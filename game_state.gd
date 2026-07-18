0d679307a142b03ff2532751651f233c097100df
oxeron2.0 <romainpierre2007@gmail.com>
2026-07-18 16:48:54 +0200

    global player scale

--- game_state.gd
+++ game_state.gd
@@ -10,7 +10,7 @@ var energy : int = 0:
 		energy_changed.emit(energy)
 var player_scale: float = 1.0:
 	set(value):
-		player_scale = clamp(value, 0.1, 2.0)
+		player_scale = clamp(value, 0.5, 2.0)
 
 
 func register_retry() -> void:
