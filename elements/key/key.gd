e562a458768cdd643b0c17eed363ef8bc9010e15
Olivier PIERRE <olivier.pierre@gmail.com>
2026-07-19 18:18:26 +0200

    Fix console error

--- elements/key/key.gd
+++ elements/key/key.gd
@@ -2,7 +2,7 @@ extends Area2D
 @onready var key: Area2D = $"."
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+
 
 var keytaken = false 
 
