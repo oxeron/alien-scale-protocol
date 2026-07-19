c9900c07273c658397c276ac8ff008bc5b3a6fa3
Olivier PIERRE <olivier.pierre@gmail.com>
2026-07-19 18:22:00 +0200

    Lint

--- doors.gd
+++ doors.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var down_door: AnimatedSprite2D = $down_door
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
-func _on_body_entered(body: PhysicsBody2D) -> void:
+func _on_body_entered(_body: PhysicsBody2D) -> void:
 	if GameState.key_taken == true : 
 		up_door.play("open")
 		down_door.play("open")
