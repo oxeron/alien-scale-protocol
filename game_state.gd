148547ade1fcfbaf16f17ade36209c1493d4c538
Olivier PIERRE <olivier.pierre@gmail.com>
2026-07-19 11:00:32 +0200

    Fix energy consumption

--- game_state.gd
+++ game_state.gd
@@ -4,17 +4,20 @@ signal energy_changed(value: int)
 
 var retry_count: int = 0
 var elapsed_time: float = 0.0
-var energy : int = 0:
+var energy : int = 100:
 	set(value):
 		energy = clamp(value, 0, 100)
 		energy_changed.emit(energy)
 var player_scale: float = 1.0:
 	set(value):
+		var previous_value = player_scale
 		player_scale = clamp(value, 0.5, 2.0)
-		print("player_scale %s" % player_scale)
+		# Consume energy only if player scale changed
+		if player_scale != previous_value:
+			GameState.energy -= 1
 
 
 func register_retry() -> void:
-	energy = 0
+	energy = 100
 	retry_count += 1
 	
