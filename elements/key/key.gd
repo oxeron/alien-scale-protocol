8d83d8d5d60742fbbd6a2a904e344d751e834a1c
oxeron2.0 <romainpierre2007@gmail.com>
2026-07-19 13:45:58 +0200

    keys

---
+++ elements/key/key.gd
@@ -0,0 +1,7 @@
+extends Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
